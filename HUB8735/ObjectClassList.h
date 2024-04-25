1f1cc0f8886aaecd782bc574e74567dd4acd15ab
mcutlab105 <168071697+mcutlab105@users.noreply.github.com>
2024-04-25 15:42:02 +0800

    Add files via upload

---
+++ HUB8735/ObjectClassList.h
@@ -0,0 +1,19 @@
+#ifndef __OBJECTCLASSLIST_H__
+#define __OBJECTCLASSLIST_H__
+
+struct ObjectDetectionItem {
+    uint8_t index;
+    const char* objectName;
+    uint8_t filter;
+};
+
+// List of objects the pre-trained model is capable of recognizing
+// Index number is fixed and hard-coded from training
+// Set the filter value to 0 to ignore any recognized objects
+ObjectDetectionItem itemList[3] = {
+    {0,  "bottle",         1},
+    {1,  "can",        1},
+    {2,  "garbage",            1},
+};
+
+#endif
